43c15c8b68f2d37f9a2e01839b27e40374fb1c92
Kd125 <kashdangar1250@gmail.com>
2023-05-15 22:12:39 -0500

    complete project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+import Beer from "./Beer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      beers: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.punkapi.com/v2/beers").then((res) => {
+      const beers = res.data;
+      this.setState({ beers });
+    });
+  }
+
+  likedBeer = (e) => {
+    console.log(e.target.checked);
+  };
+
+  render() {
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <ol>
+      //       {this.state.beers.map((beer, index) => {
+      //         return <Beer key={index} name={beer}></Beer>;
+      //       })}
+      //     </ol>
+      //   </header>
+      // </div>
+      <div className="App">
+        {this.state.beers.map((beer) => (
+          <div key={beer.id} className="css-data">
+            <h3>{beer.name}</h3>
+            <span>{beer.first_brewed}</span>
+            <h4>{beer.tagline}</h4>
+            <img src={beer.image_url} alt={beer.name} className="images" />
+            <p className="last-item">
+              <span>{beer.abv}</span>
+              {beer.description}
+            </p>
+            <input onChange={this.likedBeer} type="checkbox" />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Beer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Beer(props) {
+  return <li>{props}</li>;
+}
+
+export default Beer;
